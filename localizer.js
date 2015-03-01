ef6978942549fc406aa40e15d0c8fb93c9fc9e31
rjesh <rajesh.sitaraman@theportalgrp.com>
2015-02-28 20:37:46 -0800

    Changed to attr based dom selection

--- localizer.js
+++ localizer.js
@@ -3,15 +3,20 @@
 var Localizer = Localizer || {};
 
 Localizer.ProcessDivLocalization = function () {
-    $("div[localeval]").each(function (index) {
+    /*$("div[localeval]").each(function (index) {
         //var arr = $(this).attr('localeval').split(":");
         $(this).html(resources[$(this).attr('localeval')]);
+    });*/
+
+    $("[localeval]").each(function (index) {
+        console.log($(this).attr('localeval'));
+        $(this).html(resources[$(this).attr('localeval')]);
     });
-    $("span[localeval]").each(function (index) {
+    /*$("span[localeval]").each(function (index) {
         //var arr = $(this).attr('localeval').split(":");
         //$(this).html(resourceArr[arr[0]][arr[1]]);
         console.log($(this).attr('localeval'));
         $(this).html(resources[$(this).attr('localeval')]);
-    });
+    });*/
 };
 
